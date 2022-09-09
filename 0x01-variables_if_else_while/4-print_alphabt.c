ff8d8231cdde7ea252d85628d2a1e0bf07c61a46
ifeanyi <fadunchezor@gmail.com>
2022-09-09 19:55:26 +0100

    todays task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - program that prints the alphabet in lowercase, followed by a new line
+  *
+  * Return: Always (Success)
+  */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
