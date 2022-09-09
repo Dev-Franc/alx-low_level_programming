76b2cae1a55c8253202b185348fde6f06c12d8f6
Dev_Franc <fadunchezor@gmail.com>
2022-09-09 19:13:35 +0000

    corrections made

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - program that prints the lowercase alphabet in reverse, followed by a new line.
+  * main - prints the lowercase alphabet in reverse.
   *
   * Return: Always (Success)
   */
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - program that prints all the numbers of base 16 in lowercase, followed by a new line.
+  * main - prints all the numbers of base 16 in lowercase.
   *
   * Return: Always (Success)
   */
