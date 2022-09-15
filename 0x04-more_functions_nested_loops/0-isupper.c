e126629a1168963e236968f9a93db116c154bef1
ifeanyi <fadunchezor@gmail.com>
2022-09-15 23:41:58 +0100

    sleeping

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,15 @@
 
 /**
 * _isupper - function to check if character is uppercase
-*
 * @c: checks input of function
-*
 * Return: returns 1 if `c` is uppercase, Returns 0 otherwise 
 */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
+
 	return (1);
+
 	return (0);
 }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multipling 2 numbers
+ * @a: first param
+ * @b: second param
+ * Return: something
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
